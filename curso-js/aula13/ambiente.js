64f449e5376cd1d8f07927fe32ea7bc9e0efbcda
Caiobasf <caiosantana757@gmail.com>
2025-11-03 14:09:49 -0300

    Todos os exercicios feitos hoje

---
+++ curso-js/aula13/ambiente.js
@@ -0,0 +1,25 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+//Primeiro Testa e depois executa o código
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+} 
+*/
+
+//Primeiro executa o bloco e depois testa
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
---
+++ curso-js/aula14ex/modelo/script.js
@@ -0,0 +1,7 @@
+var inicio = document.getElementById('txtini')
+var fim = document.getElementById('txtfim')
+var passo = document.getElementById('txtpasso')
+
+function verificar() {
+    alert(inicio)
+}
